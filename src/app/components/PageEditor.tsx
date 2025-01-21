1f6ce8f26429fe145a0975356b2763ca8f89eb87
Luciano <lucianorodriguess101@gmail.com>
2025-01-20 21:45:05 -0300

    feat(components): Refactor PageEditor to support image selection, zIndex management, and improved image handling

--- src/app/components/PageEditor.tsx
+++ src/app/components/PageEditor.tsx
@@ -1,29 +1,36 @@
 import React from "react";
 import DraggableImage from "./DraggableImage";
 
-interface ImageElement {
+export interface ImageElement {
   id: string;
   src: string;
   x: number;
   y: number;
   width: number;
   height: number;
+  zIndex: number;
 }
 
 interface PageEditorProps {
   pageIndex: number;
   images: ImageElement[];
-  onUpdateImage: (img: ImageElement) => void;
+  selectedId?: string;
+  onSelectImage: (imgId: string) => void;
+  onBringToFront: (imgId: string) => void;
+  onDeleteImage: (imgId: string) => void;
+  onUpdateImage: (updated: ImageElement) => void;
   onAddImage: (file: File) => void;
-  onDeleteImage: (imageId: string) => void;
 }
 
 const PageEditor: React.FC<PageEditorProps> = ({
   pageIndex,
   images,
+  selectedId,
+  onSelectImage,
+  onBringToFront,
+  onDeleteImage,
   onUpdateImage,
   onAddImage,
-  onDeleteImage,
 }) => {
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -32,34 +39,27 @@ const PageEditor: React.FC<PageEditorProps> = ({
   };
 
   return (
-    <div className="w-[595px] h-[842px] border border-[#b1b1b1] relative my-5 mx-auto">
-      <p className="text-center absolute top-[-26px] left-1/2 transform -translate-x-1/2">
-        Página {pageIndex + 1}
-      </p>
+    <div>
+      <p>Página {pageIndex + 1}</p>
 
-      {images.map((img) => (
-        <DraggableImage
-          key={img.id}
-          src={img.src}
-          x={img.x}
-          y={img.y}
-          width={img.width}
-          height={img.height}
-          onChange={(x, y, w, h) => {
-            onUpdateImage({
-              ...img,
-              x,
-              y,
-              width: w,
-              height: h,
-            });
-          }}
-          onDelete={() => onDeleteImage(img.id)}
-        />
-      ))}
-
-      <div style={{ position: "absolute", bottom: 10, right: 10 }}>
-        <input type="file" onChange={handleFileChange} />
+      <div className="w-[595px] h-[842px] border border-gray-300 relative overflow-hidden">
+        {images.map((img) => (
+          <DraggableImage
+            key={img.id}
+            data={img}
+            isSelected={selectedId === img.id}
+            onSelect={() => onSelectImage(img.id)}
+            onBringToFront={() => onBringToFront(img.id)}
+            onDelete={() => onDeleteImage(img.id)}
+            onChange={(newX, newY, w, h) => {
+              onUpdateImage({ ...img, x: newX, y: newY, width: w, height: h });
+            }}
+          />
+        ))}
+        {/* Botão de upload no canto */}
+        <div style={{ position: "absolute", bottom: 10, right: 10 }}>
+          <input type="file" onChange={handleFileChange} />
+        </div>
       </div>
     </div>
   );
