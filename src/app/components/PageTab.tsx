581ab7efebaa2d44d3f13d0243a37a98461766fb
Luciano Souza <lucianorodriguess101@gmail.com>
2025-01-23 13:26:49 -0300

    feat(page): Enhance PageTab component to accept customizable name prop

--- src/app/components/PageTab.tsx
+++ src/app/components/PageTab.tsx
@@ -1,11 +1,15 @@
 import { FileIconSVG, TrashIconSVG } from "./svg";
 
-export function PageTab() {
+type PageTabProps = {
+  name: string;
+}
+
+export function PageTab({ name }: PageTabProps) {
   return (
-    <div className="p-4 border border-neutral-700 flex items-center justify-between rounded-2xl">
+    <div className="p-3 border border-neutral-700 flex items-center justify-between rounded-2xl">
       <div className="flex items-center gap-4">
         <FileIconSVG />
-        <p className="text-neutral-100 text-sm">Página 1</p>
+        <p className="text-neutral-300 text-base">{name}</p>
       </div>
 
       <TrashIconSVG />
