22366713cf0a36754cc6bcbdce612a23f8524241
Luciano <lucianorodriguess101@gmail.com>
2025-01-26 15:07:31 -0300

    feat(component): Enable client-side rendering for DraggableImage component

--- src/app/components/DraggableImage.tsx
+++ src/app/components/DraggableImage.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useCallback } from "react";
 import { Rnd, RndDragCallback, RndResizeCallback } from "react-rnd";
 import { ImageElement } from "./PageEditor";
