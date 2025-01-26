b8efab435f4bfed8ed26ed37522ce409e39aa840
Luciano <lucianorodriguess101@gmail.com>
2025-01-26 14:38:47 -0300

    feat(component): Update ButtonAddPage to accept onClickAddPage prop

--- src/app/components/ButtonAddPage.tsx
+++ src/app/components/ButtonAddPage.tsx
@@ -1,11 +1,20 @@
 import { AddIconSVG } from "./svg";
 
+interface ButtonAddPageProps {
+  onClickAddPage: () => void;
+}
 
-export function ButtonAddPage() {
+export function ButtonAddPage({ onClickAddPage }: ButtonAddPageProps) {
   return (
-    <div className="p-4 flex gap-2 items-center justify-center" style={{ backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='16' ry='16' stroke='%23B3B3B3FF' stroke-width='3' stroke-dasharray='8%2c 14' stroke-dashoffset='13' stroke-linecap='square'/%3e%3c/svg%3e"); border-radius: 16px` }}>
+    <div
+      className="p-4 flex gap-2 items-center justify-center hover:bg-neutral-800 transition-all duration-200 rounded-2xl cursor-pointer"
+      style={{
+        backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='16' ry='16' stroke='%23B3B3B3FF' stroke-width='3' stroke-dasharray='8%2c 14' stroke-dashoffset='13' stroke-linecap='square'/%3e%3c/svg%3e")`,
+      }}
+      onClick={onClickAddPage}
+    >
       <AddIconSVG height="24" width="24" />
       <p className="text-neutral-300 text-base">Nova página</p>
     </div>
-  )
+  );
 }
